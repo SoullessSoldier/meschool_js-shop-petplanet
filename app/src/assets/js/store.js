ffe5cfec47dcc550c84095a3545e1dcbed101667
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2024-05-14 23:55:34 +0200

    add Part2 - Day 1

---
+++ app/src/assets/js/store.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll(".store__category-button");
+
+const changeActiveBtn = ({ target }) => {
+  buttons.forEach((button) => {
+    button.classList.remove("store__category-button_active");
+  });
+  target.classList.add("store__category-button_active");
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", changeActiveBtn);
+});
